fc6342735249ffec2ccfa37442efdc9dafd8edf4
Johny Alencar <johnyguido@hotmail.com.br>
2021-05-08 00:26:57 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -6,7 +7,7 @@ import { BASE_URL } from 'utils/requests';
 type ChartData = {
 
     series: number[];
-        labels:string [];
+    labels: string[];
 
 
 }
@@ -14,22 +15,19 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    let chartData : ChartData = {labels: [], series: [] };
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response =>{
-           const data = response.data as SaleSum[];
-           const myLabels = data.map(x => x.sellerName);
-           const mySeries = data.map(x => x.sum);
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-           chartData = {labels: myLabels, series: mySeries };
-           console.log(chartData);
-        });
+                setChartData({ labels: myLabels, series: mySeries });
 
-  //  const mockData = {
-  //      series: [477138, 499928, 444867, 220426, 473088],
-  //      labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-  //  }
+            });
+    }, []);
 
     const options = {
         legend: {
@@ -41,7 +39,7 @@ const DonutChart = () => {
 
         <Chart
 
-            options={{ ...options, labels: chartData.labels}}
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="250"
